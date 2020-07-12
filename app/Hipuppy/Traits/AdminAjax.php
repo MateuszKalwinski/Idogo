39b3fac237fd608bba99659372220689054f9f5c
Mateusz-Kalwinski <mateucz27@gmail.com>
2020-07-12 23:01:14 +0200

    get breed and color for available colors for breeds

--- app/Hipuppy/Traits/AdminAjax.php
+++ app/Hipuppy/Traits/AdminAjax.php
@@ -5,14 +5,15 @@ namespace App\Hipuppy\Traits;
 use Illuminate\Http\Request;
 
 
-trait BreedAjax
+trait AdminAjax
 {
-    public function getBreeds(Request $request){
+    public function getBreeds(Request $request)
+    {
         $breeds = $this->aR->getBreeds($request);
 
         $prepare_breeds = [];
 
-        foreach ($breeds as $breed){
+        foreach ($breeds as $breed) {
             $value = [];
             $value['species_id'] = $breed['id'];
             $value['species_name'] = $breed['name'];
@@ -21,5 +22,18 @@ trait BreedAjax
         }
 
         return response()->json($prepare_breeds);
+    }
+
+    public function getColors()
+    {
+        $colors = $this->aR->getColors();
+
+        $colorCollection = $colors->map(function ($color) {
+            return collect($color->toArray())
+                ->only(['id', 'name'])
+                ->all();
+        });
+
+        return response()->json($colorCollection);
     }
 }
