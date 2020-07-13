1d8003a4c6903ce4b89ffbf9ccf0d6f799890020
Mateusz-Kalwinski <mateucz27@gmail.com>
2020-07-13 23:37:46 +0200

    fileable columns was added

--- app/AvailableColors.php
+++ app/AvailableColors.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class AvailableColors extends Model
 {
-    //
+    public $table = 'available_colors';
+    public $timestamps = true;
+    protected $fillable = ['breed_id', 'color_id', 'created_at', 'updated_at'];
 }
