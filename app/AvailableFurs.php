e17a9249c9b850c97fc4db4eb0f1497ef9c8dfa5
Mateusz-Kalwinski <mateucz27@gmail.com>
2020-08-06 22:05:44 +0200

    fillable columns in model was added

--- app/AvailableFurs.php
+++ app/AvailableFurs.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class AvailableFurs extends Model
 {
-    //
+    public $table = 'available_furs';
+    public $timestamps = true;
+    protected $fillable = ['breed_id', 'fur_id', 'created_at', 'updated_at'];
 }
