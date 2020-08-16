db443327047c53e919b0971ca130fa9c1b294c5f
Mateusz-Kalwinski <mateucz27@gmail.com>
2020-08-16 22:17:40 +0200

    fileable columns updated

--- app/AnimalSpecies.php
+++ app/AnimalSpecies.php
@@ -8,7 +8,7 @@ class AnimalSpecies extends Model
 {
     public $table = 'animal_species';
     public $timestamps = false;
-    protected $fillable = ['name', 'created_at', 'created_user_id', 'edited_at', 'edited_user_id', 'deleted_at', 'deleted_user_id'];
+    protected $fillable = ['name', 'icon', 'created_at', 'created_user_id', 'edited_at', 'edited_user_id', 'deleted_at', 'deleted_user_id'];
 
 
     public function animal_dictionary()
