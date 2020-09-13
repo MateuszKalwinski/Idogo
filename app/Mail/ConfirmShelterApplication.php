e4e1155b0d499763e1bb24841bf8fb78b1a644c4
Mateusz-Kalwinski <mateucz27@gmail.com>
2020-09-13 12:34:44 +0200

    Confirm shelter application email was added

--- app/Mail/ConfirmShelterApplication.php
+++ app/Mail/ConfirmShelterApplication.php
@@ -29,7 +29,7 @@ class ConfirmShelterApplication extends Mailable
      */
     public function build()
     {
-        return $this->subject('Hipuppy potwierdzenie aplikacji schronsika')->view('emails.ConfirmShelterApplication');
+        return $this->subject('Idogo potwierdzenie aplikacji schronsika')->view('emails.ConfirmShelterApplication');
 
     }
 }
